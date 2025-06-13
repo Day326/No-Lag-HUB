dd46799f757f96774cd086f5c6e514c045a7adeb
vthangsitink <191318345+Day326@users.noreply.github.com>
2025-06-13 18:54:24 +0800

    LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -1,9 +1,1 @@
-local function gui()
-    loadstring(game:HttpGet("https://paste.ee/r/EJjqjqGq"))()
-end
-local function farm()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/LoaderV1.lua"))()
-end
- 
-task.spawn(gui)
-task.spawn(farm)
+loadstring(game:HttpGet("https://paste.ee/r/IIBN0Gcj"))()
